796421097e5de2765c0e508cd62479ab46616d65
Pernam75 <jules.rubin@efrei.net>
2024-02-23 19:12:33 +0100

    changed project name

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-project_name = "MLOpsProject"
+project_name = "cnnClassifier"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
