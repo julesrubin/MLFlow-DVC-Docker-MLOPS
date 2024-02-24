055448d2593adc346682c3dc6a0a575c5b46e221
Pernam75 <jules.rubin@efrei.net>
2024-02-24 22:05:40 +0100

    Add model training pipeline and training configuration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion"
 
@@ -18,6 +19,17 @@ STAGE_NAME = "Prepare Base Model"
 try:
     logger.info(f"Starting {STAGE_NAME}...")
     pipeline = PrepareBaseModelTrainingPipeline()
+    pipeline.main()
+    logger.info(f"Completed {STAGE_NAME}...")
+except Exception as e:
+    logger.exception(f"Failed to run {STAGE_NAME} because of {str(e)}")
+    raise e
+
+STAGE_NAME = "Model Training"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}...")
+    pipeline = ModelTrainingPipeline()
     pipeline.main()
     logger.info(f"Completed {STAGE_NAME}...")
 except Exception as e:
