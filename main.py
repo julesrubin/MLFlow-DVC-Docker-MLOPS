45cdc356f9e3121b1e0d5dfe116b7a7c6f9d00d5
Pernam75 <jules.rubin@efrei.net>
2024-02-24 23:55:42 +0100

    Add model evaluation pipeline using MLFlow

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion"
 
@@ -30,6 +31,17 @@ STAGE_NAME = "Model Training"
 try:
     logger.info(f"Starting {STAGE_NAME}...")
     pipeline = ModelTrainingPipeline()
+    pipeline.main()
+    logger.info(f"Completed {STAGE_NAME}...")
+except Exception as e:
+    logger.exception(f"Failed to run {STAGE_NAME} because of {str(e)}")
+    raise e
+
+STAGE_NAME = "Model Evaluation"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}...")
+    pipeline = EvaluationPipeline()
     pipeline.main()
     logger.info(f"Completed {STAGE_NAME}...")
 except Exception as e:
